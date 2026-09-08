b59147ea7a978d644fb05d5445b3dd4dabcb06a5
Nuris Akbar <nuris.akbar@gmail.com>
2026-09-08 12:08:22 +0700

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (in_array(trim($slot), ['Laravel', 'Rasa Group', 'RasaGroup', 'Rasaconnect']))
+@if (str_contains(strtolower($slot), 'rasa') || str_contains(strtolower($slot), 'laravel'))
 <img src="https://rasaconnect.com/logo/Rasa%20Connect%20-%20Logo%202_Maroon%201.png" class="logo" alt="Rasaconnect Logo" style="height: 50px;">
 @else
 {{ $slot }}
